0848d65fb3e851b98cbc083db87aa65b04f5b139
Malcolm Leake <echelon1089@gmail.com>
2020-04-15 22:13:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ class Scooter {
     this.model = model
   }
 }
+
+class Name {
+  constructor(name, age, experience){
+    this.name = name
+    this.color = color
+    this.model = model
+  }
+}
+
+class Scooter {
+  constructor(year, color, model){
+    this.year = year
+    this.color = color
+    this.model = model
+  }
+}
